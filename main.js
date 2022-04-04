4dcc2272bd82e01d7da2f72d2c42ee34d3f08e76
ks-imagine <kevscaringi@gmail.com>
2022-04-03 21:39:49 -0700

    added sum data for kc

--- main.js
+++ main.js
@@ -141,7 +141,8 @@ async function getBossData(url, bossName) {
   for (var i = 0; i < data.participants.length; i++) {
     for (var j = 0; j < window.PLAYER_ARRAY.length; j++) {
       if (data.participants[i].displayName == window.PLAYER_ARRAY[j].name) {
-        window.PLAYER_ARRAY[j].bossList[bossName] = data.participants[i].progress.gained;
+        window.PLAYER_ARRAY[j].bossList[bossName] =
+          data.participants[i].progress.gained;
       }
     }
   }
@@ -178,7 +179,7 @@ function showExperienceData() {
         </tr>`;
   }
   table.innerHTML = tab;
-  sumData();
+  sumXP();
 }
 
 // Function to define innerHTML for HTML table
@@ -187,54 +188,54 @@ function showBossData() {
   let tab = `<tr>
     <th class="clickable" onclick="sortTable(0)">Player Name</th>
     <th class="clickable" onclick="sortTable(1)">Team Name</th>
-    <th class="clickable" onclick="sortTable(2)">abyssal_sire</th>
-    <th class="clickable" onclick="sortTable(3)">alchemical_hydra</th>
-    <th class="clickable" onclick="sortTable(4)">barrows_chests</th>
-    <th class="clickable" onclick="sortTable(5)">bryophyta</th>
-    <th class="clickable" onclick="sortTable(6)">callisto</th>
-    <th class="clickable" onclick="sortTable(7)">cerberus</th>
-    <th class="clickable" onclick="sortTable(8)">chambers_of_xeric</th>
-    <th class="clickable" onclick="sortTable(9)">chambers_of_xeric_challenge_mode</th>
-    <th class="clickable" onclick="sortTable(10)">chaos_elemental</th>
-    <th class="clickable" onclick="sortTable(11)">chaos_fanatic</th>
-    <th class="clickable" onclick="sortTable(12)">commander_zilyana</th>
-    <th class="clickable" onclick="sortTable(13)">corporeal_beast</th>
-    <th class="clickable" onclick="sortTable(14)">crazy_archaeologist</th>
-    <th class="clickable" onclick="sortTable(15)">dagannoth_prime</th>
-    <th class="clickable" onclick="sortTable(16)">dagannoth_rex</th>
-    <th class="clickable" onclick="sortTable(17)">dagannoth_supreme</th>
-    <th class="clickable" onclick="sortTable(18)">deranged_archaeologist</th>
-    <th class="clickable" onclick="sortTable(19)">general_graardor</th>
-    <th class="clickable" onclick="sortTable(20)">giant_mole</th>
-    <th class="clickable" onclick="sortTable(21)">grotesque_guardians</th>
-    <th class="clickable" onclick="sortTable(22)">hespori</th>
-    <th class="clickable" onclick="sortTable(23)">kalphite_queen</th>
-    <th class="clickable" onclick="sortTable(24)">king_black_dragon</th>
-    <th class="clickable" onclick="sortTable(25)">kraken</th>
-    <th class="clickable" onclick="sortTable(26)">kreearra</th>
-    <th class="clickable" onclick="sortTable(27)">kril_tsutsaroth</th>
-    <th class="clickable" onclick="sortTable(28)">mimic</th>
-    <th class="clickable" onclick="sortTable(29)">nex</th>
-    <th class="clickable" onclick="sortTable(30)">nightmare</th>
-    <th class="clickable" onclick="sortTable(31)">phosanis_nightmare</th>
-    <th class="clickable" onclick="sortTable(32)">obor</th>
-    <th class="clickable" onclick="sortTable(33)">sarachnis</th>
-    <th class="clickable" onclick="sortTable(34)">scorpia</th>
-    <th class="clickable" onclick="sortTable(35)">skotizo</th>
-    <th class="clickable" onclick="sortTable(36)">tempoross</th>
-    <th class="clickable" onclick="sortTable(37)">the_gauntlet</th>
-    <th class="clickable" onclick="sortTable(38)">the_corrupted_gauntlet</th>
-    <th class="clickable" onclick="sortTable(39)">theatre_of_blood</th>
-    <th class="clickable" onclick="sortTable(40)">theatre_of_blood_hard_mode</th>
-    <th class="clickable" onclick="sortTable(41)">thermonuclear_smoke_devil</th>
-    <th class="clickable" onclick="sortTable(42)">tzkal_zuk</th>
-    <th class="clickable" onclick="sortTable(43)">tztok_jad</th>
-    <th class="clickable" onclick="sortTable(44)">venenatis</th>
-    <th class="clickable" onclick="sortTable(45)">vetion</th>
-    <th class="clickable" onclick="sortTable(46)">vorkath</th>
-    <th class="clickable" onclick="sortTable(47)">wintertodt</th>
-    <th class="clickable" onclick="sortTable(48)">zalcano</th>
-    <th class="clickable" onclick="sortTable(49)">zulrah</th>
+    <th class="clickable" onclick="sortTable(2)">Abyssal Sire</th>
+    <th class="clickable" onclick="sortTable(3)">Alchemical Hydra</th>
+    <th class="clickable" onclick="sortTable(4)">Barrows</th>
+    <th class="clickable" onclick="sortTable(5)">Bryophyta</th>
+    <th class="clickable" onclick="sortTable(6)">Callisto</th>
+    <th class="clickable" onclick="sortTable(7)">Cerberus</th>
+    <th class="clickable" onclick="sortTable(8)">Cox</th>
+    <th class="clickable" onclick="sortTable(9)">Cox CM</th>
+    <th class="clickable" onclick="sortTable(10)">Chaos Elemental</th>
+    <th class="clickable" onclick="sortTable(11)">Chaos Fanatic</th>
+    <th class="clickable" onclick="sortTable(12)">Commander Zilyana</th>
+    <th class="clickable" onclick="sortTable(13)">Corp</th>
+    <th class="clickable" onclick="sortTable(14)">Crazy Archaeologist</th>
+    <th class="clickable" onclick="sortTable(15)">DK Prime</th>
+    <th class="clickable" onclick="sortTable(16)">DK Rex</th>
+    <th class="clickable" onclick="sortTable(17)">DK Supreme</th>
+    <th class="clickable" onclick="sortTable(18)">Deranged Archaeologist</th>
+    <th class="clickable" onclick="sortTable(19)">General Graardor</th>
+    <th class="clickable" onclick="sortTable(20)">Giant Mole</th>
+    <th class="clickable" onclick="sortTable(21)">Grotesque Guardians</th>
+    <th class="clickable" onclick="sortTable(22)">Hespori</th>
+    <th class="clickable" onclick="sortTable(23)">KQ</th>
+    <th class="clickable" onclick="sortTable(24)">KBD</th>
+    <th class="clickable" onclick="sortTable(25)">Kraken</th>
+    <th class="clickable" onclick="sortTable(26)">Kreearra</th>
+    <th class="clickable" onclick="sortTable(27)">Kril Tsutsaroth</th>
+    <th class="clickable" onclick="sortTable(28)">Mimic</th>
+    <th class="clickable" onclick="sortTable(29)">Nex</th>
+    <th class="clickable" onclick="sortTable(30)">Nightmare</th>
+    <th class="clickable" onclick="sortTable(31)">Phosanis Nightmare</th>
+    <th class="clickable" onclick="sortTable(32)">Obor</th>
+    <th class="clickable" onclick="sortTable(33)">Sarachnis</th>
+    <th class="clickable" onclick="sortTable(34)">Scorpia</th>
+    <th class="clickable" onclick="sortTable(35)">Skotizo</th>
+    <th class="clickable" onclick="sortTable(36)">Tempoross</th>
+    <th class="clickable" onclick="sortTable(37)">Gauntlet</th>
+    <th class="clickable" onclick="sortTable(38)">Corrupted Gauntlet</th>
+    <th class="clickable" onclick="sortTable(39)">ToB</th>
+    <th class="clickable" onclick="sortTable(40)">ToB Hard mode</th>
+    <th class="clickable" onclick="sortTable(41)">Thermonuclear Smoke Devil</th>
+    <th class="clickable" onclick="sortTable(42)">Zuk</th>
+    <th class="clickable" onclick="sortTable(43)">Jad</th>
+    <th class="clickable" onclick="sortTable(44)">Venenatis</th>
+    <th class="clickable" onclick="sortTable(45)">Vetion</th>
+    <th class="clickable" onclick="sortTable(46)">Vorkath</th>
+    <th class="clickable" onclick="sortTable(47)">Wintertodt</th>
+    <th class="clickable" onclick="sortTable(48)">Zalcano</th>
+    <th class="clickable" onclick="sortTable(49)">Zulrah</th>
     </tr>`;
 
   for (let p of window.PLAYER_ARRAY) {
@@ -292,10 +293,10 @@ function showBossData() {
         </tr>`;
   }
   table.innerHTML = tab;
-  // sumData();
+  sumKillCount();
 }
 
-function sumData() {
+function sumXP() {
   const table = document.getElementById("players");
 
   if (!document.getElementById("totals")) {
@@ -316,12 +317,8 @@ function sumData() {
   for (var i = 1; i < table.rows.length - 1; i++) {
     if (!table.rows[i].classList.contains("hide")) {
       totalXP += parseFloat(table.rows[i].cells[2].innerHTML.replace(/,/g, ""));
-      combatXP += parseFloat(
-        table.rows[i].cells[3].innerHTML.replace(/,/g, "")
-      );
-      skillingXP += parseFloat(
-        table.rows[i].cells[4].innerHTML.replace(/,/g, "")
-      );
+      combatXP += parseFloat(table.rows[i].cells[3].innerHTML.replace(/,/g, ""));
+      skillingXP += parseFloat(table.rows[i].cells[4].innerHTML.replace(/,/g, ""));
     }
   }
   totalXP = totalXP.toLocaleString("en-US");
@@ -329,11 +326,174 @@ function sumData() {
   skillingXP = skillingXP.toLocaleString("en-US");
 
   let tab = `<tr id="totals">
-  <th>TOTALS</th>
-  <th></th>
-  <th>Total XP: <br />${totalXP}</th>
-  <th>Combat XP: <br />${combatXP}</th>
-  <th>Skilling XP: <br />${skillingXP}</th>
+    <th>TOTALS</th>
+    <th></th>
+    <th>Total XP: <br />${totalXP}</th>
+    <th>Combat XP: <br />${combatXP}</th>
+    <th>Skilling XP: <br />${skillingXP}</th>
+  </tr>`;
+  document.getElementById("totals").innerHTML = tab;
+}
+
+function sumKillCount() {
+  const table = document.getElementById("players");
+  if (!document.getElementById("totals")) {
+    table.innerHTML += `
+    <tr id="totals">
+    </tr>
+    `;
+  }
+  let abyssal_sire=0,
+  alchemical_hydra=0,
+  barrows_chests=0,
+  bryophyta=0,
+  callisto=0,
+  cerberus=0,
+  chambers_of_xeric=0,
+  chambers_of_xeric_challenge_mode=0,
+  chaos_elemental=0,
+  chaos_fanatic=0,
+  commander_zilyana=0,
+  corporeal_beast=0,
+  crazy_archaeologist=0,
+  dagannoth_prime=0,
+  dagannoth_rex=0,
+  dagannoth_supreme=0,
+  deranged_archaeologist=0,
+  general_graardor=0,
+  giant_mole=0,
+  grotesque_guardians=0,
+  hespori=0,
+  kalphite_queen=0,
+  king_black_dragon=0,
+  kraken=0,
+  kreearra=0,
+  kril_tsutsaroth=0,
+  mimic=0,
+  nex=0,
+  nightmare=0,
+  phosanis_nightmare=0,
+  obor=0,
+  sarachnis=0,
+  scorpia=0,
+  skotizo=0,
+  tempoross=0,
+  the_gauntlet=0,
+  the_corrupted_gauntlet=0,
+  theatre_of_blood=0,
+  theatre_of_blood_hard_mode=0,
+  thermonuclear_smoke_devil=0,
+  tzkal_zuk=0,
+  tztok_jad=0,
+  venenatis=0,
+  vetion=0,
+  vorkath=0,
+  wintertodt=0,
+  zalcano=0,
+  zulrah=0;
+  for (var i = 1; i < table.rows.length - 1; i++) {
+    if (!table.rows[i].classList.contains("hide")) {
+      abyssal_sire += parseFloat(table.rows[i].cells[2].innerHTML.replace(/,/g, ""));
+      alchemical_hydra += parseFloat(table.rows[i].cells[3].innerHTML.replace(/,/g, ""));
+      barrows_chests += parseFloat(table.rows[i].cells[4].innerHTML.replace(/,/g, ""));
+      bryophyta += parseFloat(table.rows[i].cells[5].innerHTML.replace(/,/g, ""));
+      callisto += parseFloat(table.rows[i].cells[6].innerHTML.replace(/,/g, ""));
+      cerberus += parseFloat(table.rows[i].cells[7].innerHTML.replace(/,/g, ""));
+      chambers_of_xeric += parseFloat(table.rows[i].cells[8].innerHTML.replace(/,/g, ""));
+      chambers_of_xeric_challenge_mode += parseFloat(table.rows[i].cells[9].innerHTML.replace(/,/g, ""));
+      chaos_elemental += parseFloat(table.rows[i].cells[10].innerHTML.replace(/,/g, ""));
+      chaos_fanatic += parseFloat(table.rows[i].cells[11].innerHTML.replace(/,/g, ""));
+      commander_zilyana += parseFloat(table.rows[i].cells[12].innerHTML.replace(/,/g, ""));
+      corporeal_beast += parseFloat(table.rows[i].cells[13].innerHTML.replace(/,/g, ""));
+      crazy_archaeologist += parseFloat(table.rows[i].cells[14].innerHTML.replace(/,/g, ""));
+      dagannoth_prime += parseFloat(table.rows[i].cells[15].innerHTML.replace(/,/g, ""));
+      dagannoth_rex += parseFloat(table.rows[i].cells[16].innerHTML.replace(/,/g, ""));
+      dagannoth_supreme += parseFloat(table.rows[i].cells[17].innerHTML.replace(/,/g, ""));
+      deranged_archaeologist += parseFloat(table.rows[i].cells[18].innerHTML.replace(/,/g, ""));
+      general_graardor += parseFloat(table.rows[i].cells[19].innerHTML.replace(/,/g, ""));
+      giant_mole += parseFloat(table.rows[i].cells[20].innerHTML.replace(/,/g, ""));
+      grotesque_guardians += parseFloat(table.rows[i].cells[21].innerHTML.replace(/,/g, ""));
+      hespori += parseFloat(table.rows[i].cells[22].innerHTML.replace(/,/g, ""));
+      kalphite_queen += parseFloat(table.rows[i].cells[23].innerHTML.replace(/,/g, ""));
+      king_black_dragon += parseFloat(table.rows[i].cells[24].innerHTML.replace(/,/g, ""));
+      kraken += parseFloat(table.rows[i].cells[25].innerHTML.replace(/,/g, ""));
+      kreearra += parseFloat(table.rows[i].cells[26].innerHTML.replace(/,/g, ""));
+      kril_tsutsaroth += parseFloat(table.rows[i].cells[27].innerHTML.replace(/,/g, ""));
+      mimic += parseFloat(table.rows[i].cells[28].innerHTML.replace(/,/g, ""));
+      nex += parseFloat(table.rows[i].cells[29].innerHTML.replace(/,/g, ""));
+      nightmare += parseFloat(table.rows[i].cells[30].innerHTML.replace(/,/g, ""));
+      phosanis_nightmare += parseFloat(table.rows[i].cells[31].innerHTML.replace(/,/g, ""));
+      obor += parseFloat(table.rows[i].cells[32].innerHTML.replace(/,/g, ""));
+      sarachnis += parseFloat(table.rows[i].cells[33].innerHTML.replace(/,/g, ""));
+      scorpia += parseFloat(table.rows[i].cells[34].innerHTML.replace(/,/g, ""));
+      skotizo += parseFloat(table.rows[i].cells[35].innerHTML.replace(/,/g, ""));
+      tempoross += parseFloat(table.rows[i].cells[36].innerHTML.replace(/,/g, ""));
+      the_gauntlet += parseFloat(table.rows[i].cells[37].innerHTML.replace(/,/g, ""));
+      the_corrupted_gauntlet += parseFloat(table.rows[i].cells[38].innerHTML.replace(/,/g, ""));
+      theatre_of_blood += parseFloat(table.rows[i].cells[39].innerHTML.replace(/,/g, ""));
+      theatre_of_blood_hard_mode += parseFloat(table.rows[i].cells[40].innerHTML.replace(/,/g, ""));
+      thermonuclear_smoke_devil += parseFloat(table.rows[i].cells[41].innerHTML.replace(/,/g, ""));
+      tzkal_zuk += parseFloat(table.rows[i].cells[42].innerHTML.replace(/,/g, ""));
+      tztok_jad += parseFloat(table.rows[i].cells[43].innerHTML.replace(/,/g, ""));
+      venenatis += parseFloat(table.rows[i].cells[44].innerHTML.replace(/,/g, ""));
+      vetion += parseFloat(table.rows[i].cells[45].innerHTML.replace(/,/g, ""));
+      vorkath += parseFloat(table.rows[i].cells[46].innerHTML.replace(/,/g, ""));
+      wintertodt += parseFloat(table.rows[i].cells[47].innerHTML.replace(/,/g, ""));
+      zalcano += parseFloat(table.rows[i].cells[48].innerHTML.replace(/,/g, ""));
+      zulrah += parseFloat(table.rows[i].cells[49].innerHTML.replace(/,/g, ""));
+    }
+  }
+  let tab = `<tr id="totals">
+    <th>TOTALS</th>
+    <th></th>
+    <th>Abyssal Sire:<br />${abyssal_sire}</th>
+    <th>Alchemical Hydra:<br />${alchemical_hydra}</th>
+    <th>Barrows:<br />${barrows_chests}</th>
+    <th>Bryophyta:<br />${bryophyta}</th>
+    <th>Callisto:<br />${callisto}</th>
+    <th>Cerberus:<br />${cerberus}</th>
+    <th>Cox:<br />${chambers_of_xeric}</th>
+    <th>Cox CM:<br />${chambers_of_xeric_challenge_mode}</th>
+    <th>Chaos Elemental:<br />${chaos_elemental}</th>
+    <th>Chaos Fanatic:<br />${chaos_fanatic}</th>
+    <th>Commander Zilyana:<br />${commander_zilyana}</th>
+    <th>Corp:<br />${corporeal_beast}</th>
+    <th>Crazy Archaeologist:<br />${crazy_archaeologist}</th>
+    <th>DK Prime:<br />${dagannoth_prime}</th>
+    <th>DK Rex:<br />${dagannoth_rex}</th>
+    <th>DK Supreme:<br />${dagannoth_supreme}</th>
+    <th>Deranged Archaeologist:<br />${deranged_archaeologist}</th>
+    <th>General Graardor:<br />${general_graardor}</th>
+    <th>Giant Mole:<br />${giant_mole}</th>
+    <th>Grotesque Guardians:<br />${grotesque_guardians}</th>
+    <th>Hespori:<br />${hespori}</th>
+    <th>KQ:<br />${kalphite_queen}</th>
+    <th>KBD:<br />${king_black_dragon}</th>
+    <th>Kraken:<br />${kraken}</th>
+    <th>Kreearra:<br />${kreearra}</th>
+    <th>Kril Tsutsaroth:<br />${kril_tsutsaroth}</th>
+    <th>Mimic:<br />${mimic}</th>
+    <th>Nex:<br />${nex}</th>
+    <th>Nightmare:<br />${nightmare}</th>
+    <th>Phosanis Nightmare:<br />${phosanis_nightmare}</th>
+    <th>Obor:<br />${obor}</th>
+    <th>Sarachnis:<br />${sarachnis}</th>
+    <th>Scorpia:<br />${scorpia}</th>
+    <th>Skotizo:<br />${skotizo}</th>
+    <th>Tempoross:<br />${tempoross}</th>
+    <th>Gauntlet:<br />${the_gauntlet}</th>
+    <th>Corrupted Gauntlet:<br />${the_corrupted_gauntlet}</th>
+    <th>ToB:<br />${theatre_of_blood}</th>
+    <th>ToB Hard mode:<br />${theatre_of_blood_hard_mode}</th>
+    <th>Thermonuclear Smoke Devil:<br />${thermonuclear_smoke_devil}</th>
+    <th>Zuk:<br />${tzkal_zuk}</th>
+    <th>Jad:<br />${tztok_jad}</th>
+    <th>Venenatis:<br />${venenatis}</th>
+    <th>Vetion:<br />${vetion}</th>
+    <th>Vorkath:<br />${vorkath}</th>
+    <th>Wintertodt:<br />${wintertodt}</th>
+    <th>Zalcano:<br />${zalcano}</th>
+    <th>Zulrah:<br />${zulrah}</th>
   </tr>`;
   document.getElementById("totals").innerHTML = tab;
 }
@@ -369,8 +529,16 @@ function searchTable(team, column) {
       }
     }
   }
-  sortTable(2, true);
-  sumData();
+  if (bossPage) {
+    sortTable(0, false);
+  } else {
+    sortTable(2, true);
+  }
+  if (bossPage) {
+    sumKillCount();
+  } else {
+    sumXP();
+  }
 }
 
 function removeActiveTeamFilter() {
